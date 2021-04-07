de3ed2703a48f75b98c17fa88df8182ca2bce4a9
Teodor Wisniewski <teodor.wisniewski@altmansolon.com>
2021-04-07 18:41:46 +0200

    inserting staff into the table

--- PracticalSQL_NoStarch_Press/Chapter1.sql
+++ PracticalSQL_NoStarch_Press/Chapter1.sql
@@ -1,3 +1,11 @@
+--------------------------------------------------------------
+-- Practical SQL: A Beginner's Guide to Storytelling with Data
+-- by Anthony DeBarros
+
+-- First Chapter - just playing around
+--------------------------------------------------------------
+
+
 create table teachers(
 	id bigserial,
 	first_name varchar(25),
@@ -6,3 +14,19 @@ create table teachers(
 	hire_date date,
 	salary numeric
 );
+
+insert into teachers (first_name, last_name, school, hire_date, salary)
+values ('Teodor', 'Wisniewski', 'gimnazjum 3', '2021-04-01', 33000)
+
+INSERT INTO teachers (first_name, last_name, school, hire_date, salary)
+VALUES ('Janet', 'Smith', 'F.D. Roosevelt HS', '2011-10-30', 36200),
+       ('Lee', 'Reynolds', 'F.D. Roosevelt HS', '1993-05-22', 65000),
+       ('Samuel', 'Cole', 'Myers Middle School', '2005-08-01', 43500),
+       ('Samantha', 'Bush', 'Myers Middle School', '2011-10-30', 36200),
+       ('Betty', 'Diaz', 'Myers Middle School', '2005-08-30', 43500),
+       ('Kathleen', 'Roush', 'F.D. Roosevelt HS', '2010-10-22', 38500);
+
+
+
+
+select * from teachers;
